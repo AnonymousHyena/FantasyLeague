e4df314584052784a4aab430e67b976bf3eb9442
Stathis Bozikas <stathisbozikas@gmail.com>
2022-10-23 19:27:54 +0300

    Initial Build

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default (props) => {
-  return (
-    <div>
-      <footer className="footer section py-5"></footer>
-    </div>
-  );
-};
--- src/routes.js
+++
@@ -1,9 +0,0 @@
-export const AppRoutes = {
-  // pages
-  Overview: { path: "/" },
-  // Model: { path: "/model" },
-  Model: { path: "/examples/404" },
-  Teams: { path: "/teams" },
-  NotFound: { path: "/examples/404" },
-  ServerError: { path: "/examples/500" },
-};
